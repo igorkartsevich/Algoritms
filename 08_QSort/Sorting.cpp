ddff983458b68c82dde3a28336fdbed9728f7eac
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2022-12-08 21:35:33 +0300

    Ready 3

--- 08_QSort/Sorting.cpp
+++ 08_QSort/Sorting.cpp
@@ -1,1 +1,26 @@
 #include "Sorting.h"
+
+template<typename T>
+void Sorting<T>::selectionSort(std::vector<T>& data) {
+    {
+        auto startTime = clock();
+
+        int size = (int)data.size();
+
+        for (int i{}; i < size; ++i) {
+            int indexMin = i;
+            int min = data[indexMin];
+
+            for (int j{ i + 1 }; j < size; ++j)
+                if (compareTo(data[j], min, selectionSortComparisons) < 0) {
+                    indexMin = j;
+                    min = data[indexMin];
+                }
+
+            if (i != indexMin)
+                std::swap(data[i], data[indexMin]);
+        }
+
+        selectionSortExecutionTime = (float)(clock() - startTime) / CLOCKS_PER_SEC * 1000;
+    }
+}
--- 08_QSort/Sorting.h
+++ 08_QSort/Sorting.h
@@ -28,28 +28,7 @@ public:
     double quickSortExecutionTime = 0;
     double mergeSortExecutionTime = 0;
 
-    void selectionSort(std::vector<T>& data)
-    {
-        auto startTime = clock();
-        
-        int size = (int)data.size();
-
-        for (int i{}; i < size; ++i) {
-            int indexMin = i;
-            int min = data[indexMin];
-
-            for (int j{ i + 1 }; j < size; ++j)
-                if (compareTo(data[j], min, selectionSortComparisons) < 0) {
-                    indexMin = j;
-                    min = data[indexMin];
-                }
-
-            if (i != indexMin)
-                std::swap(data[i], data[indexMin]);
-        }
-
-        selectionSortExecutionTime = (float)(clock() - startTime) / CLOCKS_PER_SEC * 1000;
-    }
+    void selectionSort(std::vector<T>& data);
 
     void insertionSort(std::vector<T>& data)
     {
