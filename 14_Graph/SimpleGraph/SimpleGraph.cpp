9836589d7dbc7731a932fb15ad8daf246140b2c8
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2023-04-10 21:52:58 +0300

    SimpleGraph 1 READY

--- 14_Graph/SimpleGraph/SimpleGraph.cpp
+++ 14_Graph/SimpleGraph/SimpleGraph.cpp
@@ -1,25 +1,27 @@
 #include "SimpleGraph.h"
 #include <queue>
 
-int SimpleGraph::getImportance(std::vector<Employee*>& employees, int id) {
+Employee* getEmployeeById(int id, std::vector<Employee*>& employees) {
+	for (int i{}; i < employees.size(); ++i)
+		if (employees[i]->id == id)
+			return employees[i];
+	return nullptr;
+};
+
+int SimpleGraph::getImportance(std::vector<Employee*> employees, int id) {
 	if (employees.empty()) return 0;
+	if (!getEmployeeById(id, employees)) return 0;
 
-	int importanceSum{};
 	std::queue<Employee*> employeeQueue;
+	employeeQueue.push(getEmployeeById(id, employees));
+	int importanceSum{};
 
-	auto getEmployeeById = [&employees](int id) {
-		for (int i{}; i < employees.size(); ++i)
-			if (employees[i]->id == id)
-				return employees[i];
-	};
-
-	employeeQueue.push(getEmployeeById(id));
-	
 	while (!employeeQueue.empty()) {
 		importanceSum += employeeQueue.front()->importance;
 
 		for (auto& id : employeeQueue.front()->subordinates)
-			employeeQueue.push(getEmployeeById(id));
+			if (getEmployeeById(id, employees))
+				employeeQueue.push(getEmployeeById(id, employees));
 		
 		employeeQueue.pop();
 	}
