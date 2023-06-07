1f5ec1f9495482d3dae34b9bc48ef81dc0236ce7
igor_kartsevich <71553499+igorkartsevich@users.noreply.github.com>
2023-06-07 22:55:03 +0300

    READY

--- 15_UnobviousGraphs/src/Path.cpp
+++ 15_UnobviousGraphs/src/Path.cpp
@@ -1,6 +1,73 @@
 #include "Path.h"
 
-int Path::shortestPathDuration(const std::vector<std::vector<int>>& map)
+int Path::shortestPathDuration(const std::vector<std::vector<int>>& inMap)
 {
-	return -1;
+	Path path(inMap.size(), inMap[0].size());
+	
+	path.setNewNodeTo_nodesByMinDuration(Coordinate(0, 0), inMap[0][0]);
+	path.setNewNodeTo_mapToProcess(Coordinate(0, 0), inMap[0][0]);
+
+	while (true) {
+		Coordinate currNode = path.getCurrNode();
+		std::vector<Coordinate> nextNodesVector = path.getNextNodesVector(currNode);
+
+		for (auto& nextNode : nextNodesVector) {
+			auto currNodeDuration = path.getNodeDurationFrom_mapToProcess(currNode);
+			auto nextStepDuration = inMap[nextNode.x][nextNode.y];
+			auto nextNodeDuration = path.getNodeDurationFrom_mapToProcess(nextNode);
+
+			if (currNodeDuration + nextStepDuration < nextNodeDuration) {
+				nextNodeDuration = currNodeDuration + nextStepDuration;
+				path.setNewNodeTo_mapToProcess(nextNode, nextNodeDuration);
+
+				if (path.is_nodeLast(nextNode))
+					return path.getLastNodeDuration();
+				else
+					path.setNewNodeTo_nodesByMinDuration(nextNode, nextNodeDuration);
+			}
+		}
+	}
+}
+
+std::vector<Path::Coordinate> Path::getNextNodesVector(const Path::Coordinate& node) {
+	std::vector<Path::Coordinate> nextNodes;
+
+	if (node.x > 0)
+		nextNodes.push_back(Path::Coordinate(node.x - 1, node.y));
+	if (node.y > 0)
+		nextNodes.push_back(Path::Coordinate(node.x, node.y - 1));
+	if (node.x < xRange - 1)
+		nextNodes.push_back(Path::Coordinate(node.x + 1, node.y));
+	if (node.y < yRange - 1)
+		nextNodes.push_back(Path::Coordinate(node.x, node.y + 1));
+
+	return nextNodes;
+}
+
+void Path::setNewNodeTo_nodesByMinDuration(const Path::Coordinate& node, int nodeDuration) {
+	nodesMultyMapByDuration.emplace(nodeDuration, Path::Coordinate(node.x, node.y));
+	return;
+}
+
+void Path::setNewNodeTo_mapToProcess(const Path::Coordinate& node, int nodeDuration) {
+	mapToProcess[node.x][node.y] = nodeDuration;
+	return;
+}
+
+int Path::getNodeDurationFrom_mapToProcess(const Path::Coordinate& node) {
+	return mapToProcess[node.x][node.y];
+}
+
+Path::Coordinate Path::getCurrNode() {
+	Path::Coordinate node = begin(nodesMultyMapByDuration)->second;
+	nodesMultyMapByDuration.erase(nodesMultyMapByDuration.begin());
+	return node;
+}
+
+int Path::getLastNodeDuration() {
+	return mapToProcess[xRange - 1][yRange - 1];
+}
+
+bool Path::is_nodeLast(const Path::Coordinate& node) {
+	return node.x == xRange - 1 && node.y == yRange - 1;
 }
--- 15_UnobviousGraphs/src/Path.h
+++ 15_UnobviousGraphs/src/Path.h
@@ -1,7 +1,34 @@
 #pragma once
 #include <vector>
+#include <map>
+#include <limits>
 
 class Path {
+private:
+	struct Coordinate {
+		int x;
+		int y;
+		Coordinate(int _x, int _y) : x(_x), y(_y) {}
+		Coordinate() : Coordinate(0, 0) {}
+	};
+
+	std::vector<std::vector<int>> mapToProcess;
+	std::multimap<int, Path::Coordinate> nodesMultyMapByDuration;
+	int xRange;
+	int yRange;
+
+	std::vector<Path::Coordinate> getNextNodesVector(const Path::Coordinate& node);
+	void setNewNodeTo_nodesByMinDuration(const Path::Coordinate& node, int nodeDuration);
+	void setNewNodeTo_mapToProcess(const Path::Coordinate& node, int nodeDuration);
+	int getNodeDurationFrom_mapToProcess(const Path::Coordinate& node);
+	Path::Coordinate getCurrNode();
+	bool is_nodeLast(const Path::Coordinate& node);
+	int getLastNodeDuration();
+
 public:
-    static int shortestPathDuration(const std::vector<std::vector<int>>& map);
+	Path(int _xRange, int _yRange) : xRange(_xRange), yRange(_yRange) {
+		mapToProcess = std::vector<std::vector<int>>(_xRange, std::vector<int>(_yRange, INT_MAX));
+	}
+
+	static int shortestPathDuration(const std::vector<std::vector<int>>& map);
 };
