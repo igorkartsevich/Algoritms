6933a41bcae8b826512c045b2d9057f453857f7e
Igor Kartsevich <ikartsevich@live.com>
2023-08-02 21:17:40 +0300

    02.08

--- 18_OtherAlgorithms/Hash/Hash.cpp
+++ 18_OtherAlgorithms/Hash/Hash.cpp
@@ -23,13 +23,8 @@ std::string Hash::HashCalculator::calculateHash(const std::string& strToEncrypt)
 	for (int num512{}; num512 < data.size(); ++num512)
 		process512Block(num512);
 
-	getSum(a, initVec[0]);
-	getSum(b, initVec[1]);
-	getSum(c, initVec[2]);
-	getSum(d, initVec[3]);
-
 	std::stringstream stream;
-	stream << std::hex << a << b << c << d;
+	stream << std::hex << init_ABCD[0] << init_ABCD[1] << init_ABCD[2] << init_ABCD[3];
 
 	std::string res{};
 	stream >> res;
@@ -38,44 +33,39 @@ std::string Hash::HashCalculator::calculateHash(const std::string& strToEncrypt)
 
 void Hash::HashCalculator::setData(const std::string& str) {
 	auto len = str.length();
-	std::vector<std::vector<unsigned int>> data(1, std::vector < unsigned int>(16));
 
-	auto add_32BitsWird = [&str, &data](int index, int ch1, int ch2, int ch3, int ch4) {
-		if (index >> 4 > 0) data.push_back(std::vector<unsigned int>(16));
-		data.back()[index] = (ch1 << 24) ^ (ch2 << 16) ^ (ch3 << 8) ^ ch4;
+	auto add_32BitsWird = [&](int index, int ch1, int ch2, int ch3, int ch4) {
+		if (index % 16 == 0)
+			data.push_back(std::vector<unsigned int>(16));
+		data.back()[index % 16] = (ch1 << 24) ^ (ch2 << 16) ^ (ch3 << 8) ^ ch4;
 	};
 
-	for (int i{}; i < len / 4; ++i) // load full 32 bits block
-		add_32BitsWird(i, str[(i << 2)], str[(i << 2) + 1], str[(i << 2) + 2], str[(i << 2) + 3]);
+	for (int i{}, index{ i * 4 }; i < len / 4; ++i) // load full 32 bits block
+		add_32BitsWird(i, str[index], str[index + 1], str[index + 2], str[index + 3]);
 
-	auto remainder = len % 4; // load extra 32 bits block - remainder str + final 1;
+	auto remainder = len % 4; // load next 32 bits block - remainder str + final 1;
 	(remainder == 0)
 		? add_32BitsWird((len - remainder + 1) >> 2, 128, 0, 0, 0) :
-		(remainder == 1)
+	(remainder == 1)
 		? add_32BitsWird((len - remainder + 1) >> 2, str[len - remainder], 128, 0, 0) :
-		(remainder == 2)
+	(remainder == 2)
 		? add_32BitsWird((len - remainder + 1) >> 2, str[len - remainder], str[len - remainder + 1], 128, 0)
 		: add_32BitsWird((len - remainder + 1) >> 2, str[len - remainder], str[len - remainder + 1], str[len - remainder + 2], 128);
 
 	long long lenByBits = len * 8;
-	data.back()[14] = 0 ^ (lenByBits >> 32);
-	data.back()[15] = 0 ^ (lenByBits << 32 >> 32);
+	data.back()[15] = 0 ^ (lenByBits >> 32);
+	data.back()[14] = 0 ^ (lenByBits << 32 >> 32);
 	return;
 }
 void Hash::HashCalculator::rotateLeft_32Block(const int step) {
-	a = (a << step) | (a >> (32 - step));
+	init_ABCD[0] = (init_ABCD[0] << step) | (init_ABCD[0] >> (32 - step));
 	return;
 }
 
-void Hash::HashCalculator::rotateLeft_Vector() {
-	std::swap(a, b);
-	std::swap(a, c);
-	std::swap(a, d);
-	return;
-}
-
-void Hash::HashCalculator::getSum(unsigned int& num1, const unsigned int num2) {
-	num1 = (num1 + num2) % 4294967296;
+void Hash::HashCalculator::rotate_Vector() {
+	std::swap(init_ABCD[3], init_ABCD[0]);
+	std::swap(init_ABCD[3], init_ABCD[1]);
+	std::swap(init_ABCD[3], init_ABCD[2]);
 	return;
 }
 
@@ -90,24 +80,30 @@ void Hash::HashCalculator::process512Block(int index512) {
 	for (int roundNum{}; roundNum < 4; ++roundNum)
 		for (int num32{}; num32 < 16; ++num32) {
 
-			getSum(a, getRoundFunction(roundNum));
-
-			getSum(a, data[index512][getDataIndex(roundNum, num32)]);
-
-			getSum(a, constantsVec[roundNum][num32]);
+			init_ABCD[0]
+				+= getRoundFunction(roundNum)
+				+ data[index512][getDataIndex(roundNum, num32)]
+				+ constantsVec[roundNum][num32];
 
 			rotateLeft_32Block(stepsShiftVec[roundNum][num32]);
 
-			getSum(a, b);
+			init_ABCD[0] += init_ABCD[1];
 
-			rotateLeft_Vector();
+			rotate_Vector();
 		}
+
+	init_ABCD[0] += INIT_A;
+	init_ABCD[1] += INIT_B;
+	init_ABCD[2] += INIT_C;
+	init_ABCD[3] += INIT_D;
+
 	return;
 }
 
 unsigned int Hash::HashCalculator::getRoundFunction(const int indexRound) {
 	return
-		(indexRound == 0) ? (b & c) | (~b & d)
-		: (indexRound == 1) ? (d & b) | (~d & c)
-		: (indexRound == 2) ? b ^ c ^ d : c ^ (b | ~d);
+		(indexRound == 0) ? (init_ABCD[1] & init_ABCD[2]) | ((~init_ABCD[1]) & init_ABCD[3])
+		: (indexRound == 1) ? (init_ABCD[1] & init_ABCD[3]) | (init_ABCD[1] & (~init_ABCD[3]))
+		: (indexRound == 2) ? init_ABCD[1] ^ init_ABCD[2] ^ init_ABCD[3]
+		: init_ABCD[2] ^ (init_ABCD[1] | (~init_ABCD[3]));
 }
--- 18_OtherAlgorithms/Hash/Hash.h
+++ 18_OtherAlgorithms/Hash/Hash.h
@@ -16,43 +16,43 @@ private:
 	class HashCalculator {
 	public:
 		HashCalculator() {
-			initVec = { 19088743, 2309737967, 4275878552, 1985229328 };
+			INIT_A = 19088743;
+			INIT_B = 2309737967;
+			INIT_C = 4275878552;
+			INIT_D = 1985229328;
 
-			a = initVec[0];
-			b = initVec[1];
-			c = initVec[2];
-			d = initVec[3];
+			init_ABCD = { INIT_A, INIT_B, INIT_C, INIT_D };
 
 			stepsShiftVec = {
-				{7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22},
+				{ 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22},
 				{ 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20, 5, 9, 14, 20 },
 				{ 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23 },
 				{ 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21 }
 			};
 
-			constantsVec.resize(4, std::vector<unsigned int>(16));
-			for (int i{}; i < 4; ++i)
-				for (int j{}; j < 16; ++j)
-					constantsVec[i][j] = std::floor(std::abs((std::sin((i << 4 +j) + 1) * 4294967296)));
+			for (int i{}; i < 64; ++i) {
+				if (i % 16 == 0)
+					constantsVec.push_back(std::vector<unsigned int>(16));
+				constantsVec.back()[i % 16] = std::floor(std::abs(std::sin(i + 1) * 4294967296));
+			}
 		};
 
 		std::string calculateHash(const std::string& strToEncrypt);
 
 	private:
-		unsigned int a;
-		unsigned int b;
-		unsigned int c;
-		unsigned int d;
+		unsigned int INIT_A;
+		unsigned int INIT_B;
+		unsigned int INIT_C;
+		unsigned int INIT_D;
 		std::vector<std::vector<unsigned int>> data;
-		std::vector<unsigned int> initVec;
+		std::vector<unsigned int> init_ABCD;
 		std::vector<std::vector<int>> stepsShiftVec;
 		std::vector<std::vector<unsigned int>> constantsVec;
 
 		void setData(const std::string& str);
 		void rotateLeft_32Block(const int step);
-		void rotateLeft_Vector();
+		void rotate_Vector();
 		unsigned int getRoundFunction(const int indexRound);
-		void getSum(unsigned int& num1, const unsigned int num2);
 		int getDataIndex(const int indexRound, const int num32);
 		void process512Block(int index512);
 	};
